3b698216b96676a251c01165f18c8eccd6652ece
alaaelbana <alaaelbana58@gmail.com>
2022-11-14 23:55:26 +0200

    initial commit

--- js/custom.js
+++ js/custom.js
@@ -1,22 +1,4 @@
 new WOW().init();
-new Splide('.splide', {
-    type: 'loop',
-    autoplay: true,
-
-}).mount();
-$(window).scroll(function () {
-    if ($(window).scrollTop() > $(".products_cover").offset().top - 65) {
-        $('header').removeClass('home_header')
-    } else {
-        $('header').addClass('home_header')
-    }
-    if ($(window).scrollTop() > $(".about_us_box").offset().top) {
-        $('header').addClass('header')
-    } else {
-        $('header').removeClass('header')
-    }
-
-});
 $('.menu_btn').click(function () {
     $('header').toggleClass('home_header_hide')
     $('.header_links').toggleClass('show');
@@ -51,54 +33,3 @@ $('.products_filter_box .macaroni_filter').click(function () {
 $('.products_filter_box .sandwich_filter').click(function () {
     $('.sandwich').fadeIn()
 })
-
-
-
-
-
-
-popup = {
-    init: function () {
-        $('.gallery_img').click(function () {
-            popup.open($(this));
-        });
-
-        $(document).on('click', '.popup img', function () {
-            return false;
-        }).on('click', '.popup', function () {
-            popup.close();
-        });
-    },
-    open: function ($figure) {
-        $('.gallery').addClass('pop');
-        $popup = $('<div class=popup />').appendTo($('body'));
-        $fig = $figure.clone().appendTo($('.popup'));
-        $bg = $('<div class=bg />').appendTo($('.popup'));
-        $close = $(`
-        <div class=close>
-            <svg height="42" viewBox="0 0 48 48" width="42">
-                <path
-                    d="M38 12.83l-2.83-2.83-11.17 11.17-11.17-11.17-2.83 2.83 11.17 11.17-11.17 11.17 2.83 2.83 11.17-11.17 11.17 11.17 2.83-2.83-11.17-11.17z" />
-                <path d="M0 0h48v48h-48z" fill="none" />
-            </svg>
-        </div>`).appendTo($fig);
-        $shadow = $('<div class=shadow />').appendTo($fig);
-        src = $('img', $fig).attr('src');
-        $shadow.css({
-            backgroundImage: 'url(' + src + ')'
-        });
-        $bg.css({
-            backgroundImage: 'url(' + src + ')'
-        });
-        setTimeout(function () {
-            $('.popup').addClass('pop');
-        }, 10);
-    },
-    close: function () {
-        $('.gallery, .popup').removeClass('pop');
-        setTimeout(function () {
-            $('.popup').remove();
-        }, 100);
-    }
-};
-popup.init();
